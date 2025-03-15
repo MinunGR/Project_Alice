afb833204dcf0342d79cb7f344b09dc7a1d7271f
Fernando Barraza <fernando.jba.2000@gmail.com>
2025-03-14 22:21:39 -0600

    Emergency commit

--- Project_Alice/commands/utility/ping.js
+++ Project_Alice/commands/utility/ping.js
@@ -4,8 +4,17 @@ module.exports = {
 	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Muestra la latencia del Cliente y la API de Discord.js en milisegundos'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const message = await interaction.reply('Ping...');
+	  	const startTime = Date.now();
+	  	
+	  	await fetch('https://discord.com/api/v14', { method: 'HEAD' })
+		    .finally(() => {
+		      const clientLatency = Date.now() - startTime;
+		      const apiLatency = interaction.createdTimestamp - message.createdTimestamp;
+		      interaction.editReply(`Pong! 🏓: Client Latency: ${clientLatency} ms\nAPI Latency: ${apiLatency} ms`);
+			});
 	},
 };
+
